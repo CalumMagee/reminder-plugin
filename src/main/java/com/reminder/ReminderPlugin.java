45400aec237e316ace3f43fc624748e22b899cb6
Calum <calummagee1812@me.com>
2022-12-24 12:33:35 -0800

    Proof of concept

--- src/main/java/com/reminder/ReminderPlugin.java
+++ src/main/java/com/reminder/ReminderPlugin.java
@@ -3,14 +3,15 @@ package com.reminder;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
-import net.runelite.api.Client;
-import net.runelite.api.GameState;
+import net.runelite.api.*;
 import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.GameTick;
+import net.runelite.client.Notifier;
 import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
+
 
 @Slf4j
 @PluginDescriptor(
@@ -18,6 +19,12 @@ import net.runelite.client.plugins.PluginDescriptor;
 )
 public class ReminderPlugin extends Plugin
 {
+
+	private static final int OLM_ROOM_REGION = 12593;
+
+	@Inject
+	private Notifier notifier;
+
 	@Inject
 	private Client client;
 
@@ -36,13 +43,23 @@ public class ReminderPlugin extends Plugin
 		log.info("Example stopped!");
 	}
 
-	@Subscribe
-	public void onGameStateChanged(GameStateChanged gameStateChanged)
+	public boolean isInCoX()
 	{
-		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
-		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+		return client.getLocalPlayer() != null
+				&& OLM_ROOM_REGION == client.getLocalPlayer().getWorldLocation().getRegionID();
+	}
+
+	@Subscribe
+	public void onGameTick(GameTick tick)
+	{
+		if (isInCoX()) {
+			ItemContainer itemContainer = client.getItemContainer(InventoryID.INVENTORY);
+			//DHL
+			if (!itemContainer.contains(22978)) {
+				notifier.notify("You are missing an item!");
+			}
 		}
+
 	}
 
 	@Provides
