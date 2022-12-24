f3752ea27cf72185065d96715ba80fdc9ac7e494
Calum <calummagee1812@me.com>
2022-12-24 11:25:51 -0800

    Basic setup

--- src/main/java/com/reminder/ReminderPlugin.java
+++ src/main/java/com/reminder/ReminderPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.reminder;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class ReminderPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private ReminderConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	ReminderConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(ReminderConfig.class);
 	}
 }
